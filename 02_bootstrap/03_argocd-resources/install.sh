f875cafa0009df9a3baf6b2da58b13bf0e15025c
Julian U <48368594+lunarys@users.noreply.github.com>
2025-04-13 09:57:48 +0200

    refactor: use templating for app-of-apps

--- 02_bootstrap/03_argocd-resources/install.sh
+++ 02_bootstrap/03_argocd-resources/install.sh
@@ -1,1 +1,11 @@
-helm install argocd-apps . --namespace infra-argocd --create-namespace
+if [ -z "$1" ]; then
+  echo "Usage: $0 <test|prod>"
+  exit 1
+fi
+
+if [ "$1" != "test" ] && [ "$1" != "prod" ]; then
+  echo "Error: Invalid environment. Use 'test' or 'prod'."
+  exit 1
+fi
+
+helm install argocd-apps . --namespace infra-argocd --create-namespace --values values.yaml --values values-"$1".yaml
