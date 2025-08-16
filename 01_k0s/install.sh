42e7e59eaf52d59a05a1cf27bd42d23e611fff65
Julian U <48368594+lunarys@users.noreply.github.com>
2025-08-16 10:24:54 +0200

    fix(k0s): fix Config and ClusterConfig setup

--- 01_k0s/install.sh
+++ 01_k0s/install.sh
@@ -1,3 +1,6 @@
 #sudo ~/k0sctl-linux-amd64 apply -c k0s-test-cluster-config.yaml
 
-k0sctl apply --config k0s-cluster-config.yaml --config k0s-test-cluster.yaml
+k0sctl apply --config k0s-test-cluster.yaml --config k0s-cluster-config.yaml
+
+# TODO: --kubeconfig-out ~/.kube/config
+# TODO: --kubeconfig-cluster	Set kubernetes cluster name
